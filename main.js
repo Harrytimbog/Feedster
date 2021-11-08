a5645443c0db54284dd3228e70bd23d303b49b3c
Timilehin Arigbede <Harriemannie@gmail.com>
2021-11-08 12:28:53 +0100

    .

--- main.js
+++ main.js
@@ -6,11 +6,17 @@ $(document).ready(() => {
     $('.nav-menu').hide();
   });
 
+  // $('.btn').on('mouseenter', (event) => {
+  //   $(event.currentTarget).addClass('btn-hover')
+  //     .on('mouseleave', (event) => {
+  //       $(event.currentTarget).removeClass('btn-hover');
+  //     });
+  // });
+
   $('.btn').on('mouseenter', (event) => {
-    $(event.currentTarget).addClass('btn-hover')
-      .on('mouseleave', (event) => {
-        $(event.currentTarget).removeClass('btn-hover');
-      });
+    $(event.currentTarget).addClass('btn-hover');
+  }).on('mouseleave', (event) => {
+    $(event.currentTarget).removeClass('btn-hover');
   });
 
   $('.postText').on('keyup', (event) => {
